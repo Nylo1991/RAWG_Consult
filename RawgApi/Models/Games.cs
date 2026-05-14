68d0a2cfd7df4b0a574af962402d5d0c1fb53f90
Danilo <nylo.dk@gmail.com>
2026-05-13 21:22:19 -0300

    Adicionar arquivos de projeto.

---
+++ RawgApi/Models/Games.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawgApi.Models
+{
+    public class Games
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public string ImagemUrl { get; set; }
+
+        public string Avaliacao { get; set; } 
+
+        public string Classificação { get; set; }
+
+        public DateTime Upload { get; set; } 
+
+    }
+}
