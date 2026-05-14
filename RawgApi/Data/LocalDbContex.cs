2cb8ff7e342610a5352a1aacc9420258e0bcec20
Danilo <nylo.dk@gmail.com>
2026-05-13 21:40:18 -0300

    imprementação da classe  LocalDbContex

--- RawgApi/Data/LocalDbContex.cs
+++ RawgApi/Data/LocalDbContex.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RawgApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RawgApi.Data
 {
-    internal class LocalDbContex
+    public class LocalDbContex : DbContext
     {
+        public DbSet<Games> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            // Isso criará um arquivo chamado "meus_jogos.db" localmente
+            optionsBuilder.UseSqlite("Data Source=localdb.db");
+        }   
     }
 }
---
+++ RawgApi/Services/RawgApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawgApi.Services
+{
+    internal class RawgApiService
+    {
+    }
+}
