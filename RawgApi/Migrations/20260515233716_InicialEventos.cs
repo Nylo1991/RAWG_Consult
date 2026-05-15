6fb967768f6a133498fbc8575579d49a24e5938a
Danilo <nylo.dk@gmail.com>
2026-05-15 20:58:47 -0300

    implementando BD Local

---
+++ RawgApi/Migrations/20260515233716_InicialEventos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RawgApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialEventos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Descrição = table.Column<string>(type: "TEXT", nullable: false),
+                    ImagemUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    Avaliacao = table.Column<string>(type: "TEXT", nullable: false),
+                    Classificação = table.Column<string>(type: "TEXT", nullable: false),
+                    Upload = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
